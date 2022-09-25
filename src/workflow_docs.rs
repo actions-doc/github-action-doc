a85b54f8e2aacbc291e8ca801e0788b08e529756
Scott Rossillo <scott@rossillo.net>
2022-09-25 15:51:49 -0400

    Document callable workflows

--- src/workflow_docs.rs
+++ src/workflow_docs.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::fmt::{Formatter};
 use std::fs::File;
 use heck::ToSnakeCase;
+use indoc::indoc;
 use serde::{Deserialize};
 
 #[derive(Debug, Deserialize, PartialEq)]
@@ -18,7 +19,7 @@ pub struct GithubWorkflowInput {
 pub struct GithubWorkflowSecret {
     description: String,
     #[serde(default)]
-    required: bool,
+    required: bool
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
@@ -75,10 +76,69 @@ impl GithubWorkflowTriggerPayload {
         return mdown;
     }
 
+    fn doc_workflow_call(&self) -> String {
+        let mut mdown = String::new();
+        mdown.push_str(indoc!("
+            ### Workflow Call
+
+            This workflow can be [called](https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#workflow_call)
+            from other workflows.
+
+            #### Inputs
+
+        "));
+
+        if !self.inputs.is_empty() {
+            mdown.push_str(indoc!("
+                | Input              | Description | Type     | Required | Default |
+                |:-------------------|:------------|:---------|:---------|:--------|
+            "));
+            for (name, input) in &self.inputs {
+                let input_default = match &input.default {
+                    Some(x) => format!("`{}`", x),
+                    None => " ".to_string()
+                };
+                mdown.push_str(&format!(
+                    "| {:18} | {:18} | {:10} | {:10} | {:18} |\n",
+                    name,
+                    input.description,
+                    input.input_type,
+                    match input.required { true => "yes", false => "no" },
+                    input_default
+                ));
+            }
+        } else {
+            mdown.push_str("No inputs.")
+        }
+
+        mdown.push_str("\n\n#### Secrets\n");
+        if !self.secrets.is_empty() {
+            mdown.push_str(indoc!("
+                | Secret             | Description | Required |
+                |:-------------------|:----------------|:---------|
+            "));
+            for (name, secret) in &self.secrets {
+                mdown.push_str(&format!(
+                    "| {:18} | {:18} | {:10} |\n",
+                    name,
+                    secret.description,
+                    match secret.required { true => "yes", false => "no" }
+                ));
+            }
+        } else {
+            mdown.push_str("No secrets.")
+        }
+
+        return mdown
+    }
+
     fn to_markdown(&self, trigger: &GithubWorkflowTrigger) -> String {
         return match trigger {
             GithubWorkflowTrigger::PullRequest => {
                 self.doc_pull_request()
+            },
+            GithubWorkflowTrigger::WorkflowCall => {
+                self.doc_workflow_call()
             }
             _ => { format!("") }
         }
